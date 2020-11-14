bc29ecb5f11ca68cd58f1483a27da13f619a50d1
Azhar Rais <dev@azharaiz.com>
2020-11-14 18:38:53 +0700

    Implement user registration

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/MainActivity.kt
@@ -3,6 +3,7 @@ package id.ac.ui.cs.mobileprogramming.azharaiz.focuslist
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -26,9 +27,18 @@ class MainActivity : AppCompatActivity() {
         if (currentUser != null) {
             Log.i(TAG, "User logged in")
         } else {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+            openLoginPage()
         }
+    }
+
+    fun openLoginPage() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun logoutUser(view: View) {
+        Firebase.auth.signOut()
+        openLoginPage()
     }
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/auth/RegisterActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/auth/RegisterActivity.kt
@@ -1,12 +1,87 @@
 package id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.MainActivity
 import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.R
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private val TAG = "REGISTER_ACTIVITY"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        auth = Firebase.auth
+    }
+
+    fun registerUser(view: View) {
+        val inputEmail: EditText = findViewById(R.id.inputRegisterEmail)
+        val inputPassword: EditText = findViewById(R.id.inputRegisterPassword)
+        val inputConfirmPassword: EditText = findViewById(R.id.inputRegisterConfirmPassword)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+
+        val email: String = inputEmail.text.toString().trim()
+        val password: String = inputPassword.text.toString().trim()
+        val confirmPassword: String = inputConfirmPassword.text.toString().trim()
+
+        if (TextUtils.isEmpty(email)) {
+            inputEmail.error = "Please enter your email"
+            return
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            inputPassword.error = "Please enter your password"
+            return
+        }
+
+        if (password.length < 6) {
+            inputPassword.error = "Password too short"
+            return
+        }
+
+        if (TextUtils.isEmpty(confirmPassword)) {
+            inputConfirmPassword.error = "Please retype your password"
+            return
+        }
+
+        progressBar.visibility = View.VISIBLE
+
+
+        if (!password.equals(confirmPassword)) {
+            inputConfirmPassword.error = "Please retype correct password"
+            return
+        } else {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    progressBar.visibility = View.GONE
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
     }
 }
