f98ff34c549fd20b71746280da85d43882641d13
Azhar Rais <dev@azharaiz.com>
2020-11-14 19:09:13 +0700

    Authentication using Firebase (#1)
    * Create basic login & register layout
    
    * Implement user registration
    
    * Implement Login
    
    * Remove google-services.json

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/MainActivity.kt
@@ -1,11 +1,44 @@
 package id.ac.ui.cs.mobileprogramming.azharaiz.focuslist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.auth.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "MAIN_ACTIVITY"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            Log.i(TAG, "User logged in")
+        } else {
+            openLoginPage()
+        }
+    }
+
+    fun openLoginPage() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun logoutUser(view: View) {
+        Firebase.auth.signOut()
+        openLoginPage()
     }
 }
