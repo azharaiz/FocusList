b804c95dbd15639ccfa02f97d78edc03bdf14658
Azhar Rais <dev@azharaiz.com>
2020-11-19 05:43:59 +0700

    Feature Update Reward

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/adapters/RewardViewAdapter.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/adapters/RewardViewAdapter.kt
@@ -4,9 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.R
 import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.data.Reward
+import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.fragments.RewardListFragmentDirections
 
 class RewardViewAdapter() :
     RecyclerView.Adapter<RewardViewAdapter.RewardViewHolder>() {
@@ -17,6 +20,7 @@ class RewardViewAdapter() :
         val rewardItemTitle: TextView = view.findViewById(R.id.rewardItemTitle)
         val rewardItemStatus: TextView = view.findViewById(R.id.rewardStatus)
         val rewardItemDescription: TextView = view.findViewById(R.id.rewardItemDescription)
+        val rewardCard: CardView = view.findViewById(R.id.rewardItemCard)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RewardViewHolder {
@@ -29,6 +33,12 @@ class RewardViewAdapter() :
         val currentItem = rewardList[position]
         holder.rewardItemTitle.text = currentItem.title
         holder.rewardItemDescription.text = currentItem.description
+
+        holder.rewardCard.setOnClickListener {
+            val action = RewardListFragmentDirections
+                .actionRewardListFragmentToRewardUpdateFragment(currentItem)
+            holder.rewardCard.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount() = rewardList.size
