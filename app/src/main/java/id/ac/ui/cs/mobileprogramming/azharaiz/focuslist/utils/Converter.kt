a68abbca18b70a76fc0e6945c53c932f44db1789
Azhar Rais <dev@azharaiz.com>
2020-11-21 03:31:38 +0700

    Todo detail show duration

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/utils/Converter.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/utils/Converter.kt
@@ -8,6 +8,10 @@ object Converter {
         return if (duration == 0) "" else duration.toString()
     }
 
+    fun showDuration(duration: Int): String {
+        return "Duration: ${duration.toString()} minutes"
+    }
+
     fun toInt(string: String): Int {
         return if (string.isEmpty()) 0 else string.toInt()
     }
