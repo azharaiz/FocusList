e07b6d3842b5c9c11f7914f05a78929d487f9446
Azhar Rais <dev@azharaiz.com>
2020-11-21 13:48:10 +0700

    Settings binding viewmodel

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/fragments/SettingsFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/azharaiz/focuslist/fragments/SettingsFragment.kt
@@ -5,40 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.R
 import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.activities.TodoActivity
+import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.databinding.FragmentSettingsBinding
+import id.ac.ui.cs.mobileprogramming.azharaiz.focuslist.viewmodels.SettingsViewModel
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 class SettingsFragment : Fragment() {
-
+    private val mSettingsViewModel: SettingsViewModel by activityViewModels()
+    private lateinit var binding: FragmentSettingsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view =
-            inflater.inflate(R.layout.fragment_settings, container, false)
+
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        binding.lifecycleOwner = requireActivity()
+        binding.settingsViewModel = mSettingsViewModel
+
+        val view = binding.root
 
         val user = Firebase.auth.currentUser
 
-        user?.let {
-            view.settingsEmail.text = user.email
-            if (user.isEmailVerified) {
-                view.settingsEmailVerification.text = resources.getText(R.string.email_verified)
-                view.btnEmailVerification.visibility = View.GONE
-            } else {
-                view.settingsEmailVerification.text = resources.getText(R.string.email_not_verified)
-                view.settingsEmailVerification
-                    .setTextColor(ContextCompat.getColor(requireContext(), R.color.red))
-                view.btnEmailVerification.visibility = View.VISIBLE
-            }
-        }
+        mSettingsViewModel.userEmail.value = user!!.email
+        mSettingsViewModel.isVerified.value = user.isEmailVerified
+
 
         view.btnEmailVerification.setOnClickListener {
-            user!!.sendEmailVerification()
+            user.sendEmailVerification()
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         Toast.makeText(
